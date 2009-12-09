3111141708dd7dfc1d0dcef780cb3211b1888658
Rehno Lindeque <errantkid@gmail.com>
2009-12-09 21:57:54 +0200

    Initial commit of the poet program

---
+++ src/haskell/poet.hs
@@ -0,0 +1,38 @@
+------------------------------------------------------------------------------
+--
+--    POET.HS
+--
+--    Copyright © 2009, Rehno Lindeque. All rights reserved.
+--
+------------------------------------------------------------------------------
+module Main( main ) where
+
+{-                               DOCUMENTATION                              -}
+{-
+    IMPLEMENTATION:
+
+    TODO:
+-}
+
+{-                                 MODULES                                  -}
+-- OpenSemanticDB
+
+-- Standard modules
+import Control.Monad
+import Data.Char
+import System.IO
+import System( getArgs )
+
+
+{-                              IMPLEMENTATION                              -}
+
+main = do 
+	putStrLn "Welcome to Poet, your friendly semantic translator!"
+	args <- getArgs
+	let fileName = head args
+	putStrLn "Translating file: " 
+	fileHandle <- openFile fileName ReadMode
+	fileContents <- hGetContents fileHandle
+	putStr fileContents
+	hClose fileHandle
+
