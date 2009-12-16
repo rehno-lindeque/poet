3ef8f70d8f1144e9dc0b465a7880df54c353e1c0
Rehno Lindeque <errantkid@gmail.com>
2009-12-16 22:43:30 +0200

    Updated poet with the latest semantic db FFI

--- src/haskell/poet.hs
+++ src/haskell/poet.hs
@@ -23,18 +23,28 @@ import Control.Monad
 import Data.Char
 import System.IO
 import System( getArgs )
+import Foreign.C.String
 
 
 {-                              IMPLEMENTATION                              -}
 
 main = do 
-	putStrLn "Welcome to Poet, your friendly semantic translator!"
-	args <- getArgs
-	let fileName = head args
-	putStrLn "Translating file: " 
-	fileHandle <- openFile fileName ReadMode
-	fileContents <- hGetContents fileHandle
-	c_semanticDBInit
-	putStr fileContents
-	hClose fileHandle
+  putStrLn "Welcome to Poet, your friendly semantic translator!"
+  args <- getArgs
+  let fileName = head args
+  putStrLn "Translating file: " 
+  fileHandle <- openFile fileName ReadMode
+  fileContents <- hGetContents fileHandle
+  putStr fileContents
+  semanticDBInit
+  c_SemanticDBDebugInit
+  withCString "dom" c_DeclareOpenDomain 
+  fooSymbol <- withCString "foo" c_DeclareSymbol
+  valSymbol <- withCString "val" c_DeclareSymbol
+  c_DeclareRelation fooSymbol valSymbol
+  c_DebugOutputEnvironment
+  withCString "dom" c_CloseDomain
+  hClose fileHandle
+
+
 
