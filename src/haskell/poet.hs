a0089f22b10f23ca13dce7484a6b64a6fda9f7c7
Rehno Lindeque <errantkid@gmail.com>
2009-12-23 01:39:52 +0200

    Upgraded haskell with qsemanticdb and added some initial file loading code for the first iteration of testing

--- src/haskell/poet.hs
+++ src/haskell/poet.hs
@@ -5,6 +5,7 @@
 --    Copyright © 2009, Rehno Lindeque. All rights reserved.
 --
 ------------------------------------------------------------------------------
+{-# LANGUAGE ForeignFunctionInterface #-}
 module Main( main ) where
 
 {-                               DOCUMENTATION                              -}
@@ -17,34 +18,51 @@ module Main( main ) where
 {-                                 MODULES                                  -}
 -- OpenSemanticDB
 import OSIX.SemanticDB
+--foreign import ccall driver_callback :: IO ()
 
 -- Standard modules
-import Control.Monad
-import Data.Char
-import System.IO
 import System( getArgs )
+import System.IO
+import System.IO.Error
 import Foreign.C.String
+{-import Control.Monad
+import Data.Char
+-}
+
 
 
 {-                              IMPLEMENTATION                              -}
 
+-- Translate file
+parseLLFile :: String -> IO ()
+parseLLFile fileName = do
+  fileHandle <- (openFile fileName ReadMode)
+  fileContents <- hGetContents fileHandle
+  putStr fileContents
+  hClose fileHandle
+  putStrLn ("...File " ++ fileName ++ " closed.")
+
+-- Load a LangLang file
+loadLLFile :: [String] -> IO ()
+
+loadLLFile [] = do
+  putStrLn "No arguments supplied! Continueing with an example test case."
+  semanticDBInit
+  withCString "dom" c_DeclareOpenDomain
+  fooSymbol <- withCString "foo" c_DeclareSymbol
+  valSymbol <- withCString "val" c_DeclareSymbol
+  withCString "dom" c_CloseDomain
+  c_DebugOutputEnvironment
+  
+loadLLFile args = do
+  let fileName = head args
+  putStrLn ("Translating file: " ++ fileName ++ "..." )
+  (parseLLFile fileName) `catch` (\e -> putStrLn "Error! Could not open file for translation.")
+
+-- The main application entry-point
 main = do 
   putStrLn "Welcome to Poet, your friendly semantic translator!"
   args <- getArgs
-  let fileName = head args
-  putStrLn "Translating file: " 
-  fileHandle <- openFile fileName ReadMode
-  fileContents <- hGetContents fileHandle
-  putStr fileContents
-  semanticDBInit
-  c_SemanticDBDebugInit
-  withCString "dom" c_DeclareOpenDomain 
-  fooSymbol <- withCString "foo" c_DeclareSymbol
-  valSymbol <- withCString "val" c_DeclareSymbol
-  c_DeclareRelation fooSymbol valSymbol
-  c_DebugOutputEnvironment
-  withCString "dom" c_CloseDomain
-  hClose fileHandle
-
-
+  putStrLn (show args)
+  loadLLFile args
 
