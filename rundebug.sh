db37066deba61d4b08d161fa9c06b3a88d440ac1
Rehno Lindeque <snow@aviendha.(none)>
2010-05-30 14:57:40 +0200

    Added scripting to automatically view the debug output of poet

--- rundebug.sh
+++ rundebug.sh
@@ -1,1 +1,8 @@
+rm debugscheduler*
 cabal clean;cabal configure;cabal build;./dist/build/poet/poet data/test.src
+for file in $(ls *.dot)
+do
+  pngfile=$(echo ${file} | sed -e "s/.dot/.png/") 
+  dot -Tpng ${file} -o${pngfile}
+done
+eog debugscheduler00.png &
